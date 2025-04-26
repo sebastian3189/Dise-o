e7014b1c2d20c39d12d0df748b7513a252b34cad
Sebas <sliters83@gmail.com>
2025-04-26 00:20:28 -0500

    primera parte

---
+++ Migrations/20250426050323_eliminarunique.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GYM_ITM.Migrations
+{
+    /// <inheritdoc />
+    public partial class eliminarunique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+        name: "UQ__Horarios__CA4C08884301320E",
+        table: "Horarios");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
